8c249b88382fd7798e926394d231bf8db77a27fa
Daochen Shi <shidaochen@live.com>
2018-07-02 23:32:00 +0800

    Add project files.

---
+++ LSS_assist/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Windows.Forms;
+
+namespace LSS_assist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        DispatcherTimer dispatcherTimer;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //  DispatcherTimer setup
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            // 10ms for each tick.
+            // normally the target can get ~50 clicks per seconds, more messages are abandoned.
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+
+
+            label.Content = "Handle: " + WindowHook.FindWindow(null, "LSS").ToString();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                if (radioButtonEarn.IsChecked == true)
+                    WindowHook.SendMouseManaged("EARN");
+                if (radioButtonGive.IsChecked == true)
+                    WindowHook.SendMouseManaged("GIVE");
+            }
+            //SendKeys.SendWait("{TAB}");
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+            //progressBar1.IsIndeterminate = dispatcherTimer.IsEnabled;
+            //if (dispatcherTimer.IsEnabled)
+            //    dispatcherTimer.Start();
+            //else dispatcherTimer.Stop();
+            //
+            //label.Content = "Handle: " + WindowHook.FindWindow(null, "LSS").ToString();
+        }
+
+        private void buttonSendEnter_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowHook.SendKeyManaged(Key.Enter);
+        }
+        private void buttonSendTab_Click(object sender, RoutedEventArgs e)
+        {
+            //WindowHook.SendKeyManaged(Key.Tab);
+        }
+
+        private void radioButtonNone_Checked(object sender, RoutedEventArgs e)
+        {
+            if (radioButtonNone.IsChecked == false)
+            {
+                dispatcherTimer.Start();
+                label.Content = "Handle: " + WindowHook.FindWindow(null, "LSS").ToString();
+                // You have to make progress bar looping so that LSS window can receive messages.
+                // Otherwise, message would deliver at a very slow various rate, (1 message per sec)
+                progressBar1.IsIndeterminate = true;
+            }
+            else
+            {
+                dispatcherTimer?.Stop();
+                label.Content = "Stoppped";
+                if (progressBar1 != null)
+                    progressBar1.IsIndeterminate = false;
+            }
+        }
+    }
+}
---
+++ LSS_assist/WindowHook.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+
+namespace LSS_assist
+{
+    class WindowHook
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("User32.dll")]
+        public static extern Int32 SetForegroundWindow(int hWnd);
+
+        #region Abandoned
+        private static void SendKeyManaged(Key k)
+        {
+            if (k == Key.Enter)
+                sendKeystroke(k);
+        }
+
+        private static void sendKeystroke(Key k)
+        {
+            const uint WM_KEYDOWN = 0x100;
+            const uint WM_KEYUP = 0x101;
+            const uint WM_SYSCOMMAND = 0x018;
+            const uint SC_CLOSE = 0x053;
+
+            IntPtr WindowToFind = FindWindow(null, "LSS");
+            //SetForegroundWindow(WindowToFind.ToInt32());
+
+            //IntPtr result3 = SendMessage(WindowToFind, WM_KEYDOWN, ((IntPtr)k), (IntPtr)0);
+            //result3 = SendMessage(WindowToFind, WM_KEYUP, ((IntPtr)k), (IntPtr)0);
+            IntPtr result3 = PostMessage(WindowToFind, WM_KEYDOWN, ((IntPtr)k), (IntPtr)0);
+            result3 = PostMessage(WindowToFind, WM_KEYUP, ((IntPtr)k), (IntPtr)0);
+
+        }
+        private static void sendUsingSendKeys(string key)
+        {
+            IntPtr WindowToFind = FindWindow(null, "LSS");
+            SetForegroundWindow(WindowToFind.ToInt32());
+
+            SendKeys.SendWait(key);
+
+        }
+#endregion
+
+        public static void SendMouseManaged(string desc)
+        {
+            switch (desc.ToUpper())
+            {
+                case "EARN":
+                    sendUsingMouseSim(57, 33);
+                    break;
+                case "GIVE":
+                    sendUsingMouseSim(313, 386);
+                    break;
+
+            }
+        }
+        public static void sendUsingMouseSim(int xPos, int yPos)
+        {
+            const int WM_LBUTTONDOWN = 0x201;
+            const int WM_LBUTTONUP = 0x202;
+            const int MK_LBUTTON = 0x0001;
+
+            IntPtr WindowToFind = FindWindow(null, "LSS");
+            if (WindowToFind.ToInt32() == 0)
+                return;// Do nothing if we did not find window
+
+            IntPtr result3 = PostMessage(WindowToFind, WM_LBUTTONDOWN, (IntPtr)MK_LBUTTON, (IntPtr)(xPos + (yPos << 16)));
+            result3 = PostMessage(WindowToFind, WM_LBUTTONUP, ((IntPtr)0), (IntPtr)(xPos + (yPos << 16)));
+        }
+
+    }
+
+}
